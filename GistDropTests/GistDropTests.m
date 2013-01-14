4f067e8e53f184c0d04d13e78e4be44d5cfea0d5
Sergey Klimov <sergey.v.klimov@gmail.com>
2013-01-14 09:13:29 -0800

    more tests

--- GistDropTests/GistDropTests.m
+++ GistDropTests/GistDropTests.m
@@ -50,4 +50,27 @@
     NSLog(@"done");
 }
 
+- (void)testUploadImage
+{
+    NSString * filename = @"hello.rb";
+    NSURL* helloPath = [testBundle URLForResource:@"hello" withExtension:@"rb"];
+    NSLog(@"%@", helloPath);
+    NSError * error;
+    NSString* text = [NSString stringWithContentsOfURL:helloPath encoding:NSUTF8StringEncoding error:&error];
+    __block Gist *gist = nil;
+    [uploader postText:text filename:filename description:nil callback:^(Gist * g) {
+        gist = g;
+        [lock unlockWithCondition:1];
+    }];
+    
+    [lock lockWhenCondition:1 beforeDate:[NSDate dateWithTimeIntervalSinceNow:3]];
+    STAssertNotNil(gist, @"Gist have to be created");
+    STAssertEqualObjects(gist.filename, filename, @"suppose to be named properly");
+    NSLog(@"done");
+}
+
+
+
+
+
 @end
